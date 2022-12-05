1a72feb727e4d164fc4a87537ef627ffc49aabfb
Yousef Farge <yousef.farge@gmail.com>
2022-12-05 13:58:38 -0600

    solved day-4/part-2

--- 2022/day4/index.py
+++ 2022/day4/index.py
@@ -13,6 +13,13 @@ def isContained(a: List[int], b: List[int]):
         aStart >= bStart and aEnd <= bEnd
 
 
+def isOverlap(a: List[int], b: List[int]):
+    aStart, aEnd = a
+    bStart, bEnd = b
+
+    return aStart <= bEnd and bStart <= aEnd
+
+
 def countContainedPairs():
     count = 0
     for pair in pairs:
@@ -22,5 +29,17 @@ def countContainedPairs():
     return count
 
 
+def countOverlapedPairs():
+    count = 0
+    for pair in pairs:
+        intervalOne, intervalTwo = pair
+        if (isOverlap(intervalOne, intervalTwo)):
+            count += 1
+    return count
+
+
 print(
     f'In how many assignment pairs does one range fully contain the other?: {countContainedPairs()}')
+
+print(
+    f'In how many assignment pairs do the ranges overlap?: {countOverlapedPairs()}')
