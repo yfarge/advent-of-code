690724d05ed8450a63071bc6fba23b07df67d462
Yousef Farge <yousef.farge@gmail.com>
2022-12-04 19:22:32 -0600

    solved day-1/part-2; refactored code; changed data structure from dict to list

--- 2022/day1/index.py
+++ 2022/day1/index.py
@@ -4,26 +4,31 @@ with open("./2022/day1/input.txt") as file:
     lines = file.readlines()
 
 
-def mapElfToCalories(lines: List[str]):
-    elfToCalories = {}
+def createCalorieArray(lines: List[str]):
+    calories = []
     cur_value = 0
-    cur_elf = 1
     for line in lines:
         if (line != "\n"):
             cur_value += int(line)
             continue
-        elfToCalories[cur_elf] = cur_value
+        calories.append(cur_value)
         cur_value = 0
-        cur_elf += 1
 
-    return elfToCalories
-
-
-def maxElf(elfToCalories: Dict):
-    return max(elfToCalories.values())
+    return calories
 
 
-elfToCalories = mapElfToCalories(lines)
+def maxElfCalories(calories: List[int]):
+    return max(calories)
+
+
+def topThreeElfCalories(calories: List[int]):
+    return sum(sorted(calories, reverse=True)[:3])
+
+
+calories = createCalorieArray(lines)
 # Part 1
 print(
-    f'Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?: {maxElf(elfToCalories)}')
+    f'Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?: {maxElfCalories(calories)}')
+# Part 2
+print(
+    f'Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?: {topThreeElfCalories(calories)}')
