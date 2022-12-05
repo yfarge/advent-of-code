2fdf80930c0d67cccd807752d45d88ef6f21fbe8
Yousef Farge <yousef.farge@gmail.com>
2022-12-05 13:45:19 -0600

    solved day-4/part-1

---
+++ 2022/day4/index.py
@@ -0,0 +1,26 @@
+from typing import *
+
+with open("2022/day4/input.txt") as file:
+    pairs = [[[int(i) for i in interval.split("-")] for interval in pair.strip().split(',')]
+             for pair in file.readlines()]
+
+
+def isContained(a: List[int], b: List[int]):
+    aStart, aEnd = a
+    bStart, bEnd = b
+
+    return bStart >= aStart and bEnd <= aEnd or \
+        aStart >= bStart and aEnd <= bEnd
+
+
+def countContainedPairs():
+    count = 0
+    for pair in pairs:
+        intervalOne, intervalTwo = pair
+        if (isContained(intervalOne, intervalTwo)):
+            count += 1
+    return count
+
+
+print(
+    f'In how many assignment pairs does one range fully contain the other?: {countContainedPairs()}')
