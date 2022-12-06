0c4daf9b27461cba155b873a02ce8cf23e225ae7
Yousef Farge <yousef.farge@gmail.com>
2022-12-06 14:26:59 -0600

    solved day-6/part-2

--- 2022/day6/index.py
+++ 2022/day6/index.py
@@ -2,15 +2,20 @@ from typing import *
 
 with open("2022/day6/input.txt") as file:
     datastream = file.read()
-    assert(len(datastream) >= 4)
+    PACKET_MARKER_LENGTH = 4
+    MESSAGE_MARKER_LENGTH = 14
+    assert(len(datastream) >= MESSAGE_MARKER_LENGTH)
 
 
-def charactersProcessedBeforeFirstMarker(data: str):
-    for i in range(4, len(data)):
-        markerCandidate = set(data[i - 4: i])
-        if (len(markerCandidate) == 4):
+def charactersProcessedBeforeFirstMarker(data: str, markerLength: int):
+    for i in range(markerLength, len(data)):
+        markerCandidate = set(data[i - markerLength: i])
+        if (len(markerCandidate) == markerLength):
             return i
 
 
 print(
-    f'How many characters need to be processed before the first start-of-packet marker is detected?: {charactersProcessedBeforeFirstMarker(datastream)}')
+    f'How many characters need to be processed before the first start-of-packet marker is detected?: {charactersProcessedBeforeFirstMarker(datastream, PACKET_MARKER_LENGTH)}')
+
+print(
+    f'How many characters need to be processed before the first start-of-message marker is detected?: {charactersProcessedBeforeFirstMarker(datastream, MESSAGE_MARKER_LENGTH)}')
