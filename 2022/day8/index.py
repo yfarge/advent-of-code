f8d50a31617b62f65cf82bd1a4a9edc656409346
Yousef Farge <yousef.farge@gmail.com>
2022-12-10 15:29:23 -0600

    solved day-8/part-2

--- 2022/day8/index.py
+++ 2022/day8/index.py
@@ -1,5 +1,5 @@
 from typing import *
-
+from functools import reduce
 
 with open("day8/input.txt") as file:
     tree_map = [[eval(c) for c in [*line.strip()]]
@@ -33,5 +33,44 @@ def partOne():
     return total
 
 
+def expandInDirection(j: int, i: int, vertical_increment: int, horizontal_increment: int) -> int:
+    steps = 0
+    tree_height = tree_map[j][i]
+    y, x = j + vertical_increment, i + horizontal_increment
+    while y >= 0 and y < ROWS and x >= 0 and x < COLS:
+        if (tree_height <= tree_map[y][x]):
+            steps += 1
+            return steps
+        steps += 1
+        y += vertical_increment
+        x += horizontal_increment
+    return steps
+
+
+def expandFromCenter(j, i) -> int:
+    directions = [(i, j) for i in range(-1, 2)
+                  for j in range(-1, 2) if (abs(i) != abs(j))]
+    distances = []
+
+    for direction in directions:
+        y_inc, x_inc = direction
+        distances.append(expandInDirection(j, i, y_inc, x_inc))
+
+    return reduce(lambda x, y: x*y, distances)
+
+
+def partTwo():
+    max_scenic_score = 0
+    for j in range(1, ROWS - 1):
+        for i in range(1, COLS - 1):
+            curr_scenic_score = expandFromCenter(j, i)
+            if (curr_scenic_score > max_scenic_score):
+                max_scenic_score = curr_scenic_score
+    return max_scenic_score
+
+
 print(
     f'Consider your map; how many trees are visible from outside the grid?: {partOne()}')
+
+print(
+    f'Consider each tree on your map. What is the highest scenic score possible for any tree?: {partTwo()}')
