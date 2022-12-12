978fc219a9663b33d847f9260e0355a36318ed07
Yousef Farge <yousef.farge@gmail.com>
2022-12-12 02:06:20 -0600

    rewrote part-1 solution; attempted part-2 (not there yet)

--- 2022/day9/index.py
+++ 2022/day9/index.py
@@ -4,32 +4,21 @@ with open("day9/input.txt") as file:
     instructions = [(a, eval(b)) for a, b in [line.strip().split(' ')
                                               for line in file.readlines()]]
 
-head_position = [0, 0]
-tail_position = [0, 0]
-visitedPositions = set()
-visitedPositions.add((0, 0))
-
 X_AXIS, Y_AXIS = 0, 1
 direction2OffsetAxis = {
-    "U": (-1, Y_AXIS),
-    "L": (1, X_AXIS),
-    "D": (1, Y_AXIS),
-    "R": (-1, X_AXIS),
-
+    "U": (1, Y_AXIS),
+    "L": (-1, X_AXIS),
+    "D": (-1, Y_AXIS),
+    "R": (1, X_AXIS),
 }
 
 
-def calculateDistance(a: List[int], b: List[int]) -> complex:
+def calculateDistance(a: Tuple[int], b: Tuple[int]) -> complex:
     return pow(pow(a[0] - b[0], 2) + pow(a[1] - b[1], 2), 0.5)
 
 
-def isTouching(a: List[int], b: List[int]) -> bool:
+def isTouching(a: Tuple[int], b: Tuple[int]) -> bool:
     return calculateDistance(a, b) <= pow(2, 0.5)
-
-
-def calculateTailMove(eye: List[int], target: List[int]) -> Tuple[int, int]:
-    return ((target[0] - eye[0]) // abs(target[0] - eye[0] or 1),
-            (target[1] - eye[1]) // abs(target[1] - eye[1] or 1))
 
 
 def interpretInstruction(instruction: Tuple[str, int]) -> Union[Tuple[int, int, int], ValueError]:
@@ -38,22 +27,40 @@ def interpretInstruction(instruction: Tuple[str, int]) -> Union[Tuple[int, int, 
     return (steps, offset, axis)
 
 
-def executeInstructions() -> None:
-    for instruction in instructions:
-        steps, offset, axis = interpretInstruction(instruction)
-        while steps > 0:
-            head_position[axis] += offset
-            if (not isTouching(tail_position, head_position)):
-                dx, dy = calculateTailMove(tail_position, head_position)
-                tail_position[0] += dx
-                tail_position[1] += dy
-                visitedPositions.add(tuple(tail_position))
-            steps -= 1
+def updatePositions(positions: List[Tuple[int, int]], offset: int, axis: int):
+    new_head = list(positions[0])
+    new_head[axis] += offset
+    if (isTouching(positions[1], new_head)):
+        positions[0] = tuple(new_head)
+    else:
+        positions.insert(0, tuple(new_head))
+        positions.pop()
 
 
 def partOne() -> int:
-    return len(visitedPositions)
+    positions = [(0, 0), (0, 0)]
+    visited_positions = set([(0, 0)])
+    for instruction in instructions:
+        steps, offset, axis = interpretInstruction(instruction)
+        for _ in range(steps):
+            updatePositions(positions, offset, axis)
+            visited_positions.add(tuple(positions[-1]))
+    return len(visited_positions)
 
 
-executeInstructions()
-print(partOne())
+def partTwo() -> int:
+    positions = [tuple([0, 0]) for _ in range(10)]
+    visited_positions = set([(0, 0)])
+    for instruction in instructions:
+        steps, offset, axis = interpretInstruction(instruction)
+        for _ in range(steps):
+            updatePositions(positions, offset, axis)
+            visited_positions.add(tuple(positions[-1]))
+    return len(visited_positions)
+
+
+print(
+    f'How many positions does the tail of the rope visit at least once?: {partOne()}')
+
+print(
+    f'Simulate your complete series of motions on a larger rope with ten knots. How many positions does the tail of the rope visit at least once?: {partTwo()}')
