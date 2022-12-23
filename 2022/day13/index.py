ac75260263a457a37e946a382df6f1544d9699f8
Yousef Farge <yousef.farge@gmail.com>
2022-12-23 14:15:59 -0600

    solved day-13/part-1

--- 2022/day13/index.py
+++ 2022/day13/index.py
@@ -1,61 +1,59 @@
 from typing import *
 
 with open("day13/input.txt") as file:
-    lines = [[*line.strip()] for line in file.readlines()]
-
-
-def parsePacket(packetInput: List[str]):
-    packet = temp = []
-    stack = []
-    for char in packetInput:
-        if char.isnumeric():
-            temp.append(int(char))
-        match char:
-            case "[":
-                stack.append(temp)
-                temp.append([])
-                temp = temp[-1]
-            case "]":
-                temp = stack.pop()
-    return packet[0]
+    lines = [line.strip() for line in file.readlines()]
 
 
 def parsePacketPairStream(packetPairStream: List[List[str]]):
     pairs = []
-    for i in range(0, len(lines), 3):
-        pairs.append([parsePacket(lines[i]),
-                      parsePacket(lines[i + 1])])
+    for i in range(0, len(packetPairStream), 3):
+        pairs.append([eval(packetPairStream[i]),
+                      eval(packetPairStream[i + 1])])
     return pairs
 
 
 def matchTypes(item_one: Union[int, List], item_two: Union[int, List]):
     type_one, type_two = type(item_one), type(item_two)
-    if (type_one == int and type_two == list):
-        item_one = [item_one]
-    elif (type_two == int and type_one == list):
-        item_two = [item_two]
-    return item_one, item_two
+    if (type_one == type_two):
+        return item_one, item_two, type_one
+
+    item_one = [item_one] if type_one == int else item_one
+    item_two = [item_two] if type_two == int else item_two
+
+    return item_one, item_two, List
 
 
-def isInOrder(packet_one, packet_two):
-    i = 0
-    while i < len(packet_one) and i < len(packet_two):
-        packet_one[i], packet_two[i] = matchTypes(packet_one[i], packet_two[i])
-        type_one, type_two = type(packet_one[i]), type(packet_two[i])
+def isOrdered(packet_one, packet_two):
+    UNDECIDED = -1
 
-        # Same type check
-        if (type_one == int and type_two == int):
-            if packet_one[i] > packet_two[i]:
-                return False
-        elif (type_one == list and type_two == list):
-            if (not isInOrder(packet_one[i], packet_two[i])):
-                return False
-        i += 1
-    return True
+    def recur(l, r):
+        i = 0
+        for i in range(min(len(l), len(r))):
+            l_top, r_top, lr_type = matchTypes(l[i], r[i])
+            if (lr_type == int):
+                if (l_top != r_top):
+                    return l_top < r_top
+            else:
+                status = recur(l_top, r_top)
+                if (status != UNDECIDED):
+                    return status
+        if (len(r) == len(l)):
+            return UNDECIDED
+        return len(r) > len(l)
+
+    return recur(packet_one, packet_two)
 
 
-item_one = [9]
-item_two = [[8, 7, 6]]
+def partOne(pairs: List):
+    res = 0
+    for i, pair in enumerate(pairs, start=1):
+        l, r = pair
+        if (isOrdered(l, r)):
+            res += i
+    return res
 
-print(isInOrder(item_one, item_two))
+
 pairs = parsePacketPairStream(lines)
+
+print(
+    f'Determine which pairs of packets are already in the right order. What is the sum of the indices of those pairs?: {partOne(pairs)}')
