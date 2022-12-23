294353f6baad44de35ff6ac7e206b465629978c9
Yousef Farge <yousef.farge@gmail.com>
2022-12-23 14:58:55 -0600

    solved day-13/part-2

--- 2022/day13/index.py
+++ 2022/day13/index.py
@@ -44,7 +44,7 @@ def isOrdered(packet_one, packet_two):
     return recur(packet_one, packet_two)
 
 
-def partOne(pairs: List):
+def partOne(pairs: List) -> int:
     res = 0
     for i, pair in enumerate(pairs, start=1):
         l, r = pair
@@ -53,7 +53,40 @@ def partOne(pairs: List):
     return res
 
 
+def flattenPairs(pairs: List) -> List:
+    return [packet for pair in pairs for packet in pair]
+
+
+def sortPackets(packets: List) -> None:
+    n = len(packets)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if (not isOrdered(packets[j], packets[j+1])):
+                packets[j], packets[j+1] = packets[j+1], packets[j]
+
+
+def calculateDecoderKey(sorted_packets: List) -> int:
+    res = 1
+    for i, packet in enumerate(sorted_packets, start=1):
+        if packet == [[2]]:
+            res *= i
+        elif packet == [[6]]:
+            return res * i
+    return res
+
+
+def partTwo(pairs: List):
+    packets = flattenPairs(pairs)
+    packets.extend([[[2]], [[6]]])
+    sortPackets(packets)
+    return calculateDecoderKey(packets)
+
+
 pairs = parsePacketPairStream(lines)
 
 print(
     f'Determine which pairs of packets are already in the right order. What is the sum of the indices of those pairs?: {partOne(pairs)}')
+
+print(
+    f'Organize all of the packets into the correct order. What is the decoder key for the distress signal?: {partTwo(pairs)}'
+)
