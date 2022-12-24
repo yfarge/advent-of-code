12fa4f6710e043ff855c023293f391b7c72167da
Yousef Farge <yousef.farge@gmail.com>
2022-12-24 01:12:27 -0600

    created function to return manhattan distance

--- 2022/day15/index.py
+++ 2022/day15/index.py
@@ -7,3 +7,7 @@ with open("day15/input.txt") as file:
     for line in lines:
         positions = [int(v) for v in re.findall(r'\-?\d+', line)]
         sensor_beacon.append([tuple(positions[:2]), tuple(positions[2:])])
+
+
+def manhattanDistance(point_one: Tuple[int, int], point_two: Tuple[int, int]) -> int:
+    return sum([abs(a - b) for a, b in zip(point_one, point_two)])
