1a206ad341eafc30a3e8ff692223688648eb6e02
Yousef Farge <yousef.farge@gmail.com>
2022-12-23 22:46:24 -0600

    WIP day-14/part-1

---
+++ 2022/day14/index.py
@@ -0,0 +1,65 @@
+from typing import *
+from sys import setrecursionlimit
+
+setrecursionlimit(2200)
+
+with open("day14/input.txt") as file:
+    rock_lines = [list(map(lambda x: tuple(map(lambda v: int(v), x.strip().split(","))), split_line)) for split_lines in [[line.strip().split("->")
+                                                                                                                           for line in file.readlines()]] for split_line in split_lines]
+
+
+def clamp(v: int, min_value: int, max_value: int) -> int:
+    return max(min(v, max_value), min_value)
+
+
+def addPoints(a: Tuple[int, int], b: Tuple[int, int]) -> Tuple[int, int]:
+    return tuple(map(lambda x, y: x + y, a, b))
+
+
+def interpolatePoints(a: Tuple[int, int], b: Tuple[int, int]) -> List[Tuple[int, int]]:
+    points = [a]
+    direction_vector = [clamp(a - b, -1, 1) for a, b in zip(b, a)]
+    while a != b:
+        a = tuple(map(lambda a, b: a + b, a, direction_vector))
+        points.append(a)
+    return points
+
+
+def createInitialFilledTiles() -> Set[Tuple[int, int]]:
+    filled_tiles = set()
+    for line in rock_lines:
+        for i in range(len(line) - 1):
+            interpolated_points = interpolatePoints(
+                line[i], line[i + 1])
+            for point in interpolated_points:
+                filled_tiles.add(point)
+    return filled_tiles
+
+
+def simulateSand() -> int:
+    count = 0
+    filled_points = createInitialFilledTiles()
+    max_height = max(list(filled_points), key=lambda x: x[1])[1]
+
+    def recur(position: Tuple[int, int]):
+        nonlocal count
+        if (position[1] > max_height):
+            return
+        down = addPoints(position, (0, 1))
+        left = addPoints(position, (-1, 1))
+        right = addPoints(position, (1, 1))
+        if (down not in filled_points):
+            return recur(down)
+        elif (left not in filled_points):
+            return recur(left)
+        elif (right not in filled_points):
+            return recur(right)
+        filled_points.add(position)
+        count += 1
+        recur((500, 0))
+    recur((500, 0))
+    return count
+
+
+print(
+    f'Using your scan, simulate the falling sand. How many units of sand come to rest before sand starts flowing into the abyss below?: {simulateSand()}')
