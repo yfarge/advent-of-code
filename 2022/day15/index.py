7baae6a633e28e25fc2a62013d8310fa40d50018
Yousef Farge <yousef.farge@gmail.com>
2022-12-25 15:53:03 -0600

    WIP

--- 2022/day15/index.py
+++ 2022/day15/index.py
@@ -11,3 +11,36 @@ with open("day15/input.txt") as file:
 
 def manhattanDistance(point_one: Tuple[int, int], point_two: Tuple[int, int]) -> int:
     return sum([abs(a - b) for a, b in zip(point_one, point_two)])
+
+
+def addPoints(point_one: Tuple[int, int], point_two: Tuple[int, int]):
+    return tuple(map(lambda a, b: a + b, point_one, point_two))
+
+
+def scalePoint(scalar: int, point: Tuple[int, int]):
+    return tuple(map(lambda x: x * scalar, point))
+
+
+def addCoverage(sensor: Tuple[int, int], beacon: Tuple[int, int], coverage_map: Dict[int, Set[Tuple[int, int]]]) -> List[Tuple[int, int]]:
+    ray_length = manhattanDistance(sensor, beacon)
+    direction_vectors = [(i, j) for i in range(-1, 2) for j in range(-1, 2)]
+    grid = [["." for _ in range(25)] for _ in range(24)]
+    grid[7][8] = "S"
+
+    for scale in range(1, ray_length + 1):
+        for direction in direction_vectors:
+            point = addPoints(sensor, scalePoint(scale, direction))
+            if manhattanDistance(sensor, point) > ray_length:
+                continue
+            grid[point[1]][point[0]] = "#"
+            if point[1] not in coverage_map.keys():
+                coverage_map[point[1]] = set()
+            coverage_map[point[1]].add(point)
+
+
+coverage_map = {}
+addCoverage((8, 7), (2, 10), coverage_map)
+for sensor, beacon in sensor_beacon:
+    addCoverage(sensor, beacon, coverage_map)
+
+print(len(coverage_map[10]))
