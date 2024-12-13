31b7fd40eba96254cdc5a96495b981dd61710c22
Yousef Farge <yousef.farge@gmail.com>
2024-12-12 20:34:56 -0600

    [2024]: day09a removed swap to avoid additional memory write

--- 2024/day09a/src/main.rs
+++ 2024/day09a/src/main.rs
@@ -4,14 +4,14 @@ fn compact(v: &mut Vec<isize>) -> usize {
     let mut left = 0;
     let mut right = v.len() - 1;
     while left < right {
-        if v[right] < 0 {
-            right -= 1;
-            continue;
-        } else if v[left] >= 0 {
+        if v[left] >= 0 {
             left += 1;
             continue;
+        } else if v[right] < 0 {
+            right -= 1;
+            continue;
         } else {
-            v.swap(left, right);
+            v[left] = v[right];
             left += 1;
             right -= 1;
         }
@@ -23,15 +23,13 @@ fn compact(v: &mut Vec<isize>) -> usize {
 fn main() {
     let mut file_system = include_bytes!("../input.txt").iter().enumerate().fold(
         Vec::new(),
-        |mut acc, (index, &value)| {
-            let count = atoi::ascii_to_digit::<u8>(value).unwrap_or(0);
-            let fill = if index % 2 == 0 {
-                index.div(2) as isize
-            } else {
-                -1
-            };
-            for _ in 0..count {
-                acc.push(fill);
+        |mut acc, (index, &num)| {
+            if let Some(count) = atoi::ascii_to_digit::<usize>(num) {
+                if index % 2 == 0 {
+                    acc.extend(vec![index.div(2) as isize; count])
+                } else {
+                    acc.extend(vec![-1; count])
+                }
             }
             acc
         },
@@ -45,7 +43,7 @@ fn main() {
             .iter()
             .enumerate()
             .fold(0, |mut acc, (index, &file_id)| {
-                acc += (index as isize).mul(file_id);
+                acc += index.mul(file_id as usize);
                 acc
             })
     )
