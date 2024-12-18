14b0e9d91440425d782968ba44e9c781304911a4
Yousef Farge <yousef.farge@gmail.com>
2024-12-18 01:43:38 -0600

    [2024]: cleaned up day10 code

--- 2024/day10b/src/main.rs
+++ 2024/day10b/src/main.rs
@@ -9,6 +9,8 @@ fn main() {
     let mut queue = VecDeque::<(isize, isize)>::new();
     let mut visited = HashSet::<(isize, isize)>::new();
 
+    let directions = [(-1, 0), (0, -1), (1, 0), (0, 1)];
+
     println!(
         "{:#?}",
         (0..data.len() as isize)
@@ -25,32 +27,30 @@ fn main() {
 
                 while let Some((current_col, current_row)) = queue.pop_front() {
                     visited.insert((current_col, current_row));
-                    if let Some(current_height) = atoi::ascii_to_digit::<usize>(
-                        data[current_row as usize][current_col as usize],
-                    ) {
+                    if let Some(current_height) = data
+                        .get(current_row as usize)
+                        .and_then(|row| row.get(current_col as usize))
+                        .and_then(|&current_height| atoi::ascii_to_digit::<usize>(current_height))
+                    {
                         if current_height == 9 {
                             count += 1;
                             continue;
                         }
 
-                        for (next_col, next_row) in [
-                            (current_col - 1, current_row),
-                            (current_col, current_row - 1),
-                            (current_col + 1, current_row),
-                            (current_col, current_row + 1),
-                        ] {
-                            if next_row >= 0
-                                && next_col >= 0
-                                && next_col < data[0].len() as isize
-                                && next_row < data.len() as isize
-                                && !visited.contains(&(next_col, next_row))
+                        for &(dx, dy) in directions.iter() {
+                            let (next_col, next_row) = (current_col + dx, current_row + dy);
+
+                            if visited.contains(&(next_col, next_row)) {
+                                continue;
+                            }
+
+                            if let Some(next_height) = data
+                                .get(next_row as usize)
+                                .and_then(|row| row.get(next_col as usize))
+                                .and_then(|&next_height| atoi::ascii_to_digit::<usize>(next_height))
                             {
-                                if let Some(next_height) = atoi::ascii_to_digit::<usize>(
-                                    data[next_row as usize][next_col as usize],
-                                ) {
-                                    if next_height == current_height + 1 {
-                                        queue.push_back((next_col, next_row));
-                                    }
+                                if next_height == current_height + 1 {
+                                    queue.push_back((next_col, next_row));
                                 }
                             }
                         }
